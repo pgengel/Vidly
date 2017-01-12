b1fd00a99cde13fff10b3dbf7b1c0b72da40bc4c
Pieter Engelbrecht <pieter.engelbrecht@derivco.co.za>
2017-01-12 16:28:05 +0200

    Fundemental section done.

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customers = new List<Customer>
+            {
+                new Customer {Name = "John Smith", Id = 1},
+                new Customer {Name = "Marry Smith", Id = 2}
+            };
+
+            var viewModel = new CustomersViewModel
+            {
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+
+        [Route("customer/details/{Id}/")]
+        public ActionResult Details(int Id)
+        {
+            var customer = GetCustomers().SingleOrDefault(c => c.Id == Id);
+
+            if (customer == null)
+                return HttpNotFound();
+
+            var viewModel = new CustomersViewModel
+            {
+                Customer = customer
+            };
+
+            return View(viewModel);
+        }
+
+        private IEnumerable<Customer> GetCustomers()
+        {
+            return new List<Customer>
+             {
+                 new Customer { Id = 1, Name = "John Smith" },
+                 new Customer { Id = 2, Name = "Mary Williams" }
+             };
+        }
+    }
+}
---
+++ Vidly/ViewModels/CustomersViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Vidly.Models;
+
+namespace Vidly.ViewModels
+{
+    public class CustomersViewModel
+    {
+        public List<Customer> Customers;
+        public Customer Customer;
+    }
+}
