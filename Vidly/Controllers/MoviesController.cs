377bca07e35a763ca965914113b161598edb3d63
Pieter Engelbrecht <pieter.engelbrecht@derivco.co.za>
2017-01-12 13:29:01 +0200

    2.11 Action Params The different Action parameter in the MoviesController.cs in the webpage I can http://localhost:63988/movies?pageIndex=2
                //return View(movie)
                //return Content("Simple text")
                //return HttpNotFound()
                //return RedirectToAction("View", "Controller", new {page = 1, sortBy= "name"}) Controller/View?page=1&sortBy=name
    
    movies/edit/1 or movies/edit?1 because int RoutingCongif.cs movieId is the default BUT movies/edit?movieId=1
    
    Index(int? pageIndex, string sortBy) //The pageIndex is optional

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Web.Mvc;
 using Vidly.Models;
 
 namespace Vidly.Controllers
@@ -6,10 +7,36 @@ namespace Vidly.Controllers
     public class MoviesController : Controller
     {
         // GET: Movies/Random
-        public ActionResult Random()
+        public ActionResult Random() // Subtypes are: ViewResult -> View(),  PartialViewResult -> PartialView(), ContentResult -> Content() ie SimpleText, JsonResult -> Json(), FileResult -> Return a File, HttpNotFoundResult -> 
         {
-            var movie = new Movie() {Name = "Shrek"};
+            var movie = new Movie() { Name = "Shrek" };
+
+            //Below are the outputs of the action results
+            //return View(movie)
+            //return Content("Simple text")
+            //return HttpNotFound()
+            //return RedirectToAction("View", "Controller", new {page = 1, sortBy= "name"}) Controller/View?page=1&sortBy=name
+
             return View(movie);
+        }
+
+        public ActionResult Edit(int movieId) //movies/edit/1 or movies/edit?1 because int RoutingCongif.cs movieId is the default BUT movies/edit?movieId=1
+        {
+            return Content("movieId=" + movieId);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy) //The pageIndex is optional 
+        {
+            if (!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+            {
+                sortBy = "Name";
+            }
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
         }
     }
 }
